653a9ad13199979b15c43783860f1a85833fd6d2
Vytautas P <vytautas.prakapas@gmail.com>
2022-09-19 10:17:52 +0300

    trecia paskaita

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Genre, Book, BookInstance
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name", "display_books")
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    readonly_fields = ("id",)
+    can_delete = False
+    extra = 0
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "display_genre")
+    inlines = [BooksInstanceInline]
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ("book", "status", "due_back")
+    list_editable = ("due_back", "status")
+    list_filter = ("status", "due_back")
+    search_fields = ("id", "book__title")
+    fieldsets = (
+        ("General", {"fields": ("id", "book")}),
+        ("Availability", {"fields": ("status", "due_back")})
+    )
+
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Genre)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(BookInstance, BookInstanceAdmin)
+
--- mysite/library/models.py
+++ mysite/library/models.py
@@ -7,21 +7,30 @@ class Genre(models.Model):
                             max_length=200,
                             help_text="Iveskite knygos žanrą(pvz. detektyvas)")
 
+    class Meta:
+        verbose_name = "Žanras"
+        verbose_name_plural = "Žanrai"
+
     def __str__(self):
         return self.name
 
 class Book(models.Model):
     title = models.CharField("Pavadinimas", max_length=200)
-    author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True, related_name="books")
     summary = models.TextField("Aprasymas", max_length=1000, help_text="Trumpas knygos aprašymas")
     isbn = models.CharField("ISBN", max_length=13)
-    genre = models.ManyToManyField(Genre, help_text="Išrinkite žantą(us) šiai knygai")
+    genre = models.ManyToManyField(Genre, help_text="Išrinkite žanrą(us) šiai knygai")
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[str(self.id)])
+
+    def display_genre(self):
+        return ", ".join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = "Žanras"
 
 class Author(models.Model):
     first_name = models.CharField("Vardas", max_length=100)
@@ -32,6 +41,11 @@ class Author(models.Model):
 
     def get_absolute_url(self):
         return reverse("author-detail", args=[str(self.id)])
+
+    def display_books(self):
+        return ", ".join(book.title for book in self.books.all()[:3])
+
+    display_books.short_description = "Knygos"
 
     def __str__(self):
         return f"{self.last_name} {self.first_name}"
