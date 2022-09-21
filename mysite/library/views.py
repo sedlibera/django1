1306eb11422a2822f4f7148bb7da100c1fe9f1c0
Vytautas P <vytautas.prakapas@gmail.com>
2022-09-21 09:49:51 +0300

    trecios paskaitos pabaiga

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, Author, BookInstance, Genre
 
 def index(request):
-    return HttpResponse("Labas, pasauli! Pirmas Django appsas!")
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status="g").count()
+    # num_instances_available = BookInstance.objects.filter(status__exact="g").count()
+    num_authors = Author.objects.count()
 
+    kontext = {"num_books": num_books, "num_instances": num_instances,
+               "num_instances_available": num_instances_available,
+               "num_authors": num_authors}
+
+    return render(request, "index.html", context=kontext)
+
+def authors(request):
+
+    autoriai = Author.objects.all()
+    kontext = {
+        "authors": autoriai
+    }
+    return render(request, "authors.html", context=kontext)
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('library/', include('library.urls')),
+    path('', RedirectView.as_view(url="library/", permanent=True)),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
